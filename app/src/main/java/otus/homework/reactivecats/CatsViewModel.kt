55c62f64b9f3f2da17c46bb5685899b7e9466567
avereshchaga <avereshchaga@gmail.com>
2025-01-30 15:29:06 +0300

    * 2. Изменена логика в `CatsViewModel.kt` с колбеков на RX

--- app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
+++ app/src/main/java/otus/homework/reactivecats/CatsViewModel.kt
@@ -1,13 +1,12 @@
 package otus.homework.reactivecats
 
 import android.content.Context
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.PublishSubject
 
 class CatsViewModel(
     catsService: CatsService,
@@ -15,8 +14,7 @@ class CatsViewModel(
     context: Context
 ) : ViewModel() {
 
-    private val _catsLiveData = MutableLiveData<Result>()
-    val catsLiveData: LiveData<Result> = _catsLiveData
+    val catsSubject: PublishSubject<Result> = PublishSubject.create()
     private var disposable: Disposable? = null
 
     init {
@@ -24,10 +22,8 @@ class CatsViewModel(
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                { fact -> _catsLiveData.value = Success(fact) },
-                { e ->
-                    _catsLiveData.value = Error(e.message ?: "Error")
-                }
+                { fact -> catsSubject.onNext(Success(fact)) },
+                { e -> catsSubject.onNext(Error(e.message ?: "Error")) }
             )
     }
 
